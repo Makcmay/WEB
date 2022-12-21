1bf29b4fadc917098909af83d41f1a7c270f550b
MakcMay <makcmay@gmail.com>
2022-12-21 09:07:19 +0300

    resave

--- HW3/2.js
+++ HW3/2.js
@@ -6,3 +6,27 @@ nameHi(userName);
 function nameHi(name) {
     console.log(`Привет, ${name}!`);
 }
+
+
+// // 'use strict';
+// function getName(){
+//     const userName = prompt('Введите ваше имя: ');
+//     while (true){
+//         if (userName == /^[0-9A-ZА-ЯЁ]+$/i.test(userName)) {
+//             return userName;
+//         }
+//         alert('Некорректный ввод')
+//         userName = prompt('Введите ваше имя: ');   
+//     }
+// }
+
+// // const userName = prompt('Введите ваше имя: '); 
+// // const isAlphaNumeric = userName => /^[0-9A-ZА-ЯЁ]+$/i.test(str)
+
+// function nameHi(name) {
+//     console.log(`Привет, ${name}!`);
+// }
+
+// // const nameU = getName();
+// nameHi(nameU);
+
